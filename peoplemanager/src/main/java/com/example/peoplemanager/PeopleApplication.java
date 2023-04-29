74f2f3147402d98a9bc441417221146869451c50
GGFalco <beppe682@gmail.com>
2023-04-29 14:52:20 +0200

    handle new person and edit person

--- peoplemanager/src/main/java/com/example/peoplemanager/PeopleApplication.java
+++ peoplemanager/src/main/java/com/example/peoplemanager/PeopleApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class PeopleApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("person-overview.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(PeopleApplication.class.getResource("person-overview.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Person Manager");
         stage.setScene(scene);
         stage.show();
     }
@@ -20,4 +20,7 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
+
 }
